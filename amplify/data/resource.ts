5cd505a2d24ec41988fee85bf5eabff5e8142edc
kohcharles <charles@creatual.io>
2025-09-04 22:45:51 +0800

    unique auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -10,8 +10,8 @@ const schema = a.schema({
   Todo: a
     .model({
       content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+      }).authorization(allow => [allow.owner()]),
+    
 });
 
 export type Schema = ClientSchema<typeof schema>;
@@ -19,10 +19,12 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: 'userPool',    
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
+    
+    
   },
 });
 
